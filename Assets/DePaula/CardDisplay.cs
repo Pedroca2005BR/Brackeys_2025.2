6f9b30a6993f953b153ff1848b4ae61e2ec6a5a8
Pedro de Paula <pedrodepaula2005@gmail.com>
2025-08-27 18:01:13 -0300

    Test Card working as intended

--- Assets/DePaula/CardDisplay.cs
+++ Assets/DePaula/CardDisplay.cs
@@ -1,9 +1,29 @@
+using TMPro;
 using UnityEngine;
 
 public class CardDisplay : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI nameComponent;
+    [SerializeField] TextMeshProUGUI descriptionComponent;
+    
+
     public void DisplayCard(Card card)
     {
+        nameComponent.text = card.name;
+        descriptionComponent.text = card.description;
+    }
 
+
+    // Efeito quando o mouse estiver emcima da carta
+    public void Hovering()
+    {
+        Debug.Log("Hovering!");
+    }
+
+
+    // Quando o player clicar na carta
+    public void Click()
+    {
+        Debug.Log("Chosen!");
     }
 }
